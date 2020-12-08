6c7c6403827ab03add9d034570e441c135ab3d23
Steven McKeown <stevenmckeown19@gmail.com>
2020-12-08 00:54:14 +0000

    Initial Express App

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
@@ -8,7 +9,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
-    console.info(`Server running at ${port}`);
+  console.info(`Server running at ${port}`);
 });
