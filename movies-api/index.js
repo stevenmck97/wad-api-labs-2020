5785138cd3692ecbadab018e3213757279351a7b
Steven McKeown <stevenmckeown19@gmail.com>
2021-01-03 16:18:55 +0000

    Week 9 refactor Movie api

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,15 +3,15 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 
-//configure body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
@@ -19,3 +19,4 @@ app.use('/api/movies', moviesRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+
